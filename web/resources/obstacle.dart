f47f778713a1aae857737d5fe689ae33e98215ec
tom <tom.ka3@gmail.com>
2019-10-06 00:29:08 +0300

    Initial obstacle work

---
+++ web/resources/obstacle.dart
@@ -0,0 +1,25 @@
+import 'dart:html';
+import 'package:tuple/tuple.dart';
+import '../player.dart';
+import '../utils.dart';
+
+class Obstacle {
+  ImageElement img;
+  Tuple2<num, num> dimensions; // anchored by bottom-right point
+  bool is_actionable;
+
+  Obstacle(this.img, this.dimensions, this.is_actionable);
+  void draw(CanvasRenderingContext2D ctx, Location loc) {
+    ctx.drawImageScaled(img, loc.x * TILE_SIZE, loc.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+  }
+
+  void do_action(Player player) {}
+}
+
+class StaticObstacle extends Obstacle {
+  StaticObstacle(ImageElement img, Tuple2<num, num> dimensions) : super(img, dimensions, false);
+}
+
+StaticObstacle make_house() {
+  return StaticObstacle(ImageElement(src: 'resources/house_large.bmp'), Tuple2<num, num>(2, 2));
+}
