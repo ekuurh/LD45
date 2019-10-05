6dab3b4ecc8966b0afc7286d4c294dc7f5740944
az <az@a.com>
2019-10-05 19:35:20 +0300

    Center and scale canvas

--- web/ld45.dart
+++ web/ld45.dart
@@ -13,12 +13,10 @@ void main() async {
   canvas = querySelector('#canvas');
   ctx = canvas.getContext('2d');
 
-  Map map = new Map([[make_blue_tile(), make_red_tile()], [make_red_tile(), make_blue_tile()]]);
-  map.tiles[0][1].is_walkable = false;
-  map.draw(ctx);
-  print(how_to_get_to(Location(0, 0), Location(1, 1), map));
-
   World world = World(Level());
+  TILE_SIZE = (canvas.width / world.map.width).round();
+
+  
 
   document.onKeyDown.listen(world.player.handle_keydown);
   document.onKeyUp.listen(world.player.handle_keyup);
