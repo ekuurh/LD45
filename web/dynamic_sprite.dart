5323537b1a4c960948ab75687e1ed2dda2b82dda
tom <tom.ka3@gmail.com>
2019-10-06 18:23:44 +0300

    Initial dynamic sprite stuff

--- web/dynamic_sprite.dart
+++ web/dynamic_sprite.dart
@@ -1,15 +1,30 @@
 import 'dart:html';
 import 'utils.dart';
 
-class DynamicSprite : Drawable{
+class DynamicSprite extends Drawable{
   List<ImageElement> frames;
+  Location dimensions;
   num framerate;
 
-  num _last_change_time;
+  num _time_until_swap;
   num _curr_frame;
 
-  DynamicSprite(List<ImageElement> t_frames, {num t_framerate=4}) {
+  DynamicSprite(List<ImageElement> t_frames, Location t_dimensions, {num t_framerate=4}) {
     frames = t_frames;
+    dimensions = t_dimensions;
     framerate = t_framerate;
+  }
+
+  void draw(CanvasRenderingContext2D ctx, Location loc) {
+    ctx.drawImageScaled(frames[_curr_frame], (loc.x - dimensions.x + 1)*TILE_SIZE, (loc.x - dimensions.y + 1)*TILE_SIZE,
+        dimensions.x * TILE_SIZE, dimensions.y * TILE_SIZE);
+  }
+
+  void update(num dt) {
+    _time_until_swap -= dt;
+    if(_time_until_swap <= 0.0) {
+      _time_until_swap = 1.0/framerate;
+      _curr_frame = (_curr_frame + 1) % frames.length;
+    }
   }
 }
--- web/obstacle.dart
+++ web/obstacle.dart
@@ -91,6 +91,9 @@ List<Tuple2<Obstacle, Location>> obstacles_from_string(String s) {
           line++;
           col = -1;
         }} break;
+      case " ":
+        col -= 1;
+        break;
       default:
         {
           print("Obstacle parsing error - unrecognized character '${ch}'!");
